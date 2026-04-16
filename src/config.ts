576467dca9688588714640fcfa043186a361e2a5
honglei <striplettt1445017782@outlook.com>
2026-04-16 14:23:48 +0800

    no banner

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: true,
+		enable: false,
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
@@ -74,7 +74,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Honglei He",
-	bio: "兴趣使然的网文作家，冲浪达人，前端工程师~",
+	bio: "兴趣使然的网文作家，前端工程师~",
 	links: [
 		{
 			name: "Twitter",
